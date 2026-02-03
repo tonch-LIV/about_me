e9d1f102e7e012a6718691e96824d691cc5416ca
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-02-03 14:58:42 -0800

    liked app.js; wrote first question, pushing to test functionality

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # about_me
 
-02.03.2026
-will contain lorem text on main index.html page, until being updated, while asking the user 5 questions using javascript.
+- 02.03.2026  
+  - will contain lorem text on main index.html page, until being updated, while asking the user 5 questions using javascript.
+  - added lorem and over all structure for future sections (biography, pictures, etc.)
---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// show confirm dialog pop-up with a question
+// let locks = confirm("Does Antonio have black hair?");
+//   if (locks) {
+//       // user clicked OK (Yes)
+//       alert("too long, needs a cut");
+//   } else {
+//       // user clicked Cancel (No)
+//       alert("hmmm... lets look again");
+//   };
+
+let locks = confirm('Does Antonio have black hair?');
+console.log('Antonio has ' + locks + ' hair.g' );
+alert('His hair is ' + locks + '!');
