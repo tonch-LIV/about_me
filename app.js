01d17f4562c119a6249542d93f2379e2e8de2c34
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-02-03 15:21:38 -0800

    fixed syntax and formatting, test 3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict'
 
-// show confirm dialog pop-up with a question
-// let locks = confirm("Does Antonio have black hair?");
-//   if (locks) {
-//       // user clicked OK (Yes)
-//       alert("too long, needs a cut");
-//   } else {
-//       // user clicked Cancel (No)
-//       alert("hmmm... lets look again");
-//   };
+// let locks = confirm('Does Antonio have black hair?');
+// console.log(locks + ', his hair is black!' );
+// alert(locks + '!');
 
-let locks = confirm('Does Antonio have black hair?');
-console.log(locks + ', his hair is black!' );
-alert(locks + '!');
+// show confirm dialog pop-up with a question
+let locks = confirm("Does Antonio have dark hair?");
+  if(locks) {// user clicked OK (Yes)
+    alert("yes, dark colored hair and quite long as well, needs a cut...");
+  } else {// user clicked Cancel (No)
+    alert("hmmm.. let's have another gander");
+  };
+console.log(locks + '! Antonio has dark colored hair.');
+
